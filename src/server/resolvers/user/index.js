2d643d1900d8bf179ead85d408f878b915e57722
Rui Fernandes <ruifernandes@sioslife.com>
2019-11-18 13:39:30 +0000

    Implement subscription on user creation

--- src/server/resolvers/user/index.js
+++ src/server/resolvers/user/index.js
@@ -1,13 +1,25 @@
 import User from '../../../models/user';
+import PubSub from '../pub-sub';
+
+const USER_ADDED_TOPIC = 'userAdded';
 
 export default {
   Query: {
     user: (parent, args) => User.findById(args.id),
-    users: (parent, args) => User.find({}),
+    users: (parent, args) => User.find({})
   },
   Mutation: {
-    addUser: (parent, args) => User.create(args),
+    addUser: (parent, args) =>
+      User.create(args).then(user => {
+        PubSub.publish(USER_ADDED_TOPIC, { [USER_ADDED_TOPIC]: user });
+        return user;
+      }),
     deleteUser: (parent, args) => User.removeById(args.id),
-    updateUser: (parent, args) => User.updateById(args),
+    updateUser: (parent, args) => User.updateById(args)
   },
+  Subscription: {
+    userAdded: {
+      subscribe: () => PubSub.asyncIterator(USER_ADDED_TOPIC)
+    }
+  }
 };
